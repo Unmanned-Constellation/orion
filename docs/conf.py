b8a4041074ed810d9f896926322fb9a56bbf0a58
Nathan Harris <nate.harris60@gmail.com>
2026-05-25 02:48:27 +0000

    ci(workflows): add docs workflow

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,26 @@
 project = "Orion"
 author = "Unmanned Constellation"
-extensions = ["breathe", "myst_parser"]
+
+# Added 'exhale' to the extensions
+extensions = [
+    "breathe", 
+    "myst_parser",
+    "exhale"
+]
 
 breathe_projects = {"orion": "_build/doxygen/xml"}
 breathe_default_project = "orion"
+
+# Exhale configuration
+exhale_args = {
+    "containmentFolder":     "./api",
+    "rootFileName":          "library_root.rst",
+    "doxygenStripFromPath":  "..",
+    "rootFileTitle":         "Orion API Reference",
+    "createTreeView":        True,
+    "exhaleExecutesDoxygen": False,
+    "exhaleUseDoxyfile":     True
+}
 
 html_theme = "furo"
 exclude_patterns = ["_build", "requirements.txt"]
---
+++ docs/index.md
@@ -0,0 +1,39 @@
+# Orion
+
+```{toctree}
+:maxdepth: 1
+:caption: Developer Guide
+
+development-environment
+building-and-testing
+documentation
+testing
+ci-cd
+dependency-management
+coding-standards
+cross-compilation
+versioning
+orin-setup
+```
+
+```{toctree}
+:maxdepth: 1
+:caption: Architecture Decision Records
+:glob:
+
+adr/*
+```
+
+```{toctree}
+:maxdepth: 2
+:caption: API Reference
+
+api/library_root
+```
+
+```{toctree}
+:maxdepth: 1
+:caption: Libraries
+
+libs/orion-clock
+```
